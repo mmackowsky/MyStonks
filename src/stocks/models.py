a84a8ede495c6a7f68ee73505bd4f9bd0321415c
mmackowsky <maciostah@gmail.com>
2024-07-24 20:37:28 +0200

    feat(models): update fields

--- src/stocks/models.py
+++ src/stocks/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from users.models import Profile
+
 
 class DailyProfit(models.Model):
     date = models.DateField()
     profit = models.FloatField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
     def __repr__(self):
         return f"{self.date}; {self.user} - {self.profit}"
